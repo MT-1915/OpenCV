b56ee31e2f9f4d3532a9357157f696709569bcaa
mete5050 <33295066+mete5050@users.noreply.github.com>
2019-01-15 17:49:30 +0300

    Update yuztani3.py

--- yuztani3.py
+++ yuztani3.py
@@ -50,13 +50,18 @@ while True:
 
     for(x,y,w,h) in faces:
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
         if(id == 2):
-            print("Suclu")
+            print("Suclu",int(confidence))
+            b=0
+            g=0
+            r=255
         if(id == 1):
             print("Suclu Degil")
+            b=0
+            g=255
+            r=0
+        cv2.rectangle(img, (x,y), (x+w,y+h), (b,g,r), 2)
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
             id = names[id]
@@ -65,8 +70,8 @@ while True:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
         
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (b,g,r), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (b,g,r), 1)  
     
     cv2.imshow('camera',img) 
 
